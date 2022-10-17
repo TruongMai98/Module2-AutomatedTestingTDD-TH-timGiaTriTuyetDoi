b9e7604fabfd89735da9fd53178e2ebaefc3ff74
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-17 23:08:12 +0700

    add test number = 1 expected = 1

--- src/AbsoluteNumberCalculatorTest.java
+++ src/AbsoluteNumberCalculatorTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,11 +7,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class AbsoluteNumberCalculatorTest {
 
     @Test
-    void findAbsolute() {
+    @DisplayName("number = 0, expected = 0")
+    void findAbsolute0() {
         int number = 0;
         int expected = 0;
 
         int result = AbsoluteNumberCalculator.findAbsolute(number);
-        assertEquals(expected,result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("number = 1, number = 1")
+    void findAbsolute1() {
+        int number = 1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
     }
 }
