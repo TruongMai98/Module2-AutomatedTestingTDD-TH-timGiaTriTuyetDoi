edf02d22fe900c21cd6d8d7a3418e3f4d6a11120
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-17 23:27:00 +0700

    refactor test number = -1 expected = 1

--- src/AbsoluteNumberCalculator.java
+++ src/AbsoluteNumberCalculator.java
@@ -1,9 +1,5 @@
 public class AbsoluteNumberCalculator {
     public static int findAbsolute(int number){
-      if (number < 0) {
-          return - number;
-      } else {
-          return number;
-      }
+        return (number < 0) ? - number : number;
     }
 }
